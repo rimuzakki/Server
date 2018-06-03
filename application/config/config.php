280ccada49dd34a7ea317e05fc45b171ff022304
rimuzakki <rimuzakki@gmail.com>
2018-06-03 10:39:00 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/server/';
+
+//Setingan Folder Assets dan Folder didalamnya
+$config['assets']			= $config['base_url'].'assets/';
+
 
 /*
 |--------------------------------------------------------------------------
